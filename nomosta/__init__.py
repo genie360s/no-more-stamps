fc0ca585868cff60cb65339c1dedf52447f866b1
genie360s <alexgmkwizu@gmail.com>
2024-01-27 22:49:30 +0300

    create authentication

---
+++ nomosta/__init__.py
@@ -0,0 +1,37 @@
+import os
+from flask import Flask
+
+def create_app(test_config=None):
+    """Creates and configure an instance of the Flask application."""
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path,'nomosta.sqlite'),
+    )
+
+    if test_config is None:
+        # Load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py',silent=True)
+    else:
+        # Load the test config if passed in
+        app.config.from_mapping(test_config)
+    
+    # Ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass    
+
+    # A simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+    #importing and calling the function init_app from db.py
+    from . import db
+    db.init_app(app)
+
+    #importing and registering the blueprint from auth.py
+    from . import auth
+    app.register_blueprint(auth.bp)
+    
+    return app
---
+++ nomosta/auth.py
@@ -0,0 +1,96 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from nomosta.db import get_db
+
+bp = Blueprint('auth',__name__,url_prefix='/auth')
+
+@bp.route('/register',methods=('GET','POST'))
+def register():
+    """Registers a new user. Validates that the username is not already taken. Hashes the password for security."""
+    if request.method == 'POST':
+        fullname = request.form['fullname']
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        
+        if not fullname:
+            error = 'Fullname is required.'
+        elif not username:
+            error = 'Username is required.'
+        elif not email:
+            error = 'Email is required.'
+        elif not password:
+            error = 'Password is required.'
+        
+        if error is None:
+            try:
+                db.execute('INSERT INTO user (fullname,username,email,password) VALUES (?,?,?,?)',(fullname,username,email,generate_password_hash(password)))
+                db.commit()
+            except db.IntegrityError:
+                error = f"User {username} is already registered."
+            else:
+                return redirect(url_for('auth.login'))
+        
+        flash(error)
+    
+    return render_template('auth/register.html')
+
+@bp.route('/login',methods=('GET','POST'))
+def login():
+    """Logs in a registered user by adding the user id to the session."""
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        
+        user = db.execute('SELECT * FROM user WHERE email = ?',(email,)).fetchone()
+        
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'],password):
+            error = 'Incorrect password.'
+        
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('auth.login'))
+        
+        flash(error)
+    
+    return render_template('auth/login.html')
+
+@bp.before_app_request
+def load_logged_in_user():
+    """If a user id is stored in the session, load the user object from the database into g.user."""
+    user_id = session.get('user_id')
+    
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute('SELECT * FROM user WHERE id = ?',(user_id,)).fetchone()
+
+@bp.route('/logout')
+def logout():
+    """Clears the current session, including the stored user id."""
+    session.clear()
+    return redirect(url_for('index'))
+
+def login_required(view):
+    """Decorator that redirects anonymous users to the login page."""
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        
+        return view(**kwargs)
+    
+    return wrapped_view
